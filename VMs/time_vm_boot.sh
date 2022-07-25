8c97a2c22e423de5b4a6a914af54bf386757c0b9
John Harrigan <jharriga@redhat.com>
2022-07-25 09:41:22 -0400

    template - requires addition of VM start cmdline

---
+++ VMs/time_vm_boot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Logs timestamp prior to starting VM
+
+# Do any pre-VM start work here
+
+# Records timestamp to temp file
+TS=$(date +%s.%N)
+echo -n "$TS" > /tmp/vm_start_time
+
+# Start the VM
+# < Insert VM start cmdline HERE >
+
+# VM has been powered off. Report start time and clean-up
+echo "start time: $TS"
+rm /tmp/vm_start_time
