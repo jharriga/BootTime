b7a271a07832e35c151477274363ff350d0c5941
John Harrigan <jharriga@redhat.com>
2022-08-16 15:36:16 -0400

    Create tar_results.sh

---
+++ systemd_unitfile/tar_results.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Produces boot timing artifacts for most recent boot
+
+# Prompt user for (prepended) filename label
+echo "Enter the test results label name: "
+read label
+
+if ls "$label"* 1> /dev/null 2>&1; then
+    echo "Please choose a unique label - exiting"
+    exit 1
+fi
+
+# Get current date-time
+curr_ts=$(date "+%Y.%m.%d-%H.%M.%S")
+
+# Create list of results filenames
+DMESG="${label}.dmesg"
+TIME="${label}.time"
+DOT="${label}.dot"
+PLOT="${label}.plot"
+ALLRESULTS="$DMESG $TIME $DOT $PLOT"
+TARBALL="${label}.${curr_ts}.tar"
+
+# Record 'dmesg' results
+dmesg | grep -m1 -i "myapp" > "$DMESG"
+
+# Record ‘systemd-analyze time’ results
+systemd-analyze time > "$TIME"
+
+# Create ‘systemd-analyze dot’ graph
+echo "producing DOT"
+systemd-analyze dot myapp.service > "$DOT"
+
+# Create ‘systemd-analyze plot’ results
+echo "producing PLOT"
+systemd-analyze plot > "$PLOT"
+
+# Package results into tarball (verbosely)
+echo "creating TARBALL with results files"
+tar -cvf $TARBALL $ALLRESULTS
+
+if [ -f $TARBALL ]; then
+	echo "SUCCESS: Created $TARBALL"
+	echo "Cleaning up results files"
+	rm -f $ALLRESULTS
+else
+	echo "FAILURE: $TARBALL not created"
+	echo "Did not cleanup results files"
+fi
