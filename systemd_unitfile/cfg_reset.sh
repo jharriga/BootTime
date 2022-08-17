77030fa7aaca0aa935bf2313108ae80ec015c846
John Harrigan <jharriga@redhat.com>
2022-08-17 12:42:46 -0400

    Create cfg_reset.sh

---
+++ systemd_unitfile/cfg_reset.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# RESET
+
+UNITFILE=/etc/systemd/system/myapp.service
+UNITDIR=/etc/systemd/system/myapp.service.d
+
+if [ -f "$UNITFILE" ] || [ -d "$UNITDIR" ]; then
+    echo "Unit file already exists."
+    while true; do
+        read -p "Remove (y/n): " response
+	case $response in
+	    [Yy]* ) rm -f $UNITFILE; rm -rf $UNITDIR; break;;
+	    [Nn]* ) echo "exiting"; exit;;
+	    * ) echo "Please answer yes or no";;
+        esac
+    done
+else
+    echo "Unit file not found, exiting"
+    exit 1
+fi
+
+# Verify
+systemctl daemon-reload
+systemctl cat myapp.service
+echo "Previous cmd should have returned Not Found"
+
+exit 0
