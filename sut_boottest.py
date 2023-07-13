856d7045bf284ed3ce8472ac03607ac067f57a42
John Harrigan <jharriga@redhat.com>
2023-07-13 17:03:50 -0400

    increased reboot 'delay' value from 5s to 60s

--- sut_boottest.py
+++ sut_boottest.py
@@ -381,7 +381,7 @@ def phase3(ip, usr, passwd):
     et_reboot.start()
 
     # Need to stall/pause while shutdown completes
-    delay = 5           # just a guess, pause for reboot to complete
+    delay = 60           # just a guess, pause for reboot to complete
     time.sleep(delay)   # just a guess...
 
     # Start ssh timer
